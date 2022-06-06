aa27da04c80b7286486de885b4753523e3280be2
ruiined <ruiined@gmail.com>
2022-06-06 04:48:17 +0100

    Filtering by organisation

--- src/startlist/startlist.tsx
+++ src/startlist/startlist.tsx
@@ -8,6 +8,7 @@ const Startlist = () => {
   const [error, setError] = useState<string>("");
   const [sort, setSort] = useState<string>("");
   const [order, setOrder] = useState<string>("asc");
+  const [filters, setFilters] = useState<any>({});
 
   const loadEntries = () => {
     axios
@@ -17,6 +18,9 @@ const Startlist = () => {
       })
       .catch((error) => setError(error.message));
   };
+
+  const handleOrgFilter = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    setFilters({ ...filters, organiserTitle: event.target.value });
 
   const handleSort = (event: React.ChangeEvent<HTMLSelectElement>) =>
     setSort(event.target.value);
@@ -42,6 +46,14 @@ const Startlist = () => {
           a[sort] === b[sort] ? 0 : a[sort] < b[sort] ? position : position * -1
         );
 
+  const filteredEntries = !filters.organiserTitle
+    ? entries
+    : entries
+        .slice()
+        .filter((entry) => entry.organiserTitle === filters.organiserTitle);
+
+  const organisers = [...new Set(entries.map((entry) => entry.organiserTitle))];
+
   if (!entries) return <h2>No entries</h2>;
   if (error) return <h2>{error}</h2>;
 
@@ -55,8 +67,15 @@ const Startlist = () => {
         order={order}
         clearSort={clearSort}
       />
+      <select onChange={handleOrgFilter} name="filter" id="filter">
+        {organisers.map((org, i) => (
+          <option value={org} key={i}>
+            {org}
+          </option>
+        ))}
+      </select>
       <div>
-        {sortedEntries.map((entry, i) => (
+        {filteredEntries.map((entry, i) => (
           <div key={i}>
             <Entry entry={entry} />
           </div>
