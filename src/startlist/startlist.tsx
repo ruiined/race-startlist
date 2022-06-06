ff2a3c81082cc56bbcf41dd0dda3ec1256e65710
ruiined <ruiined@gmail.com>
2022-06-06 02:42:35 +0100

    All the relevant data is now being displayed on the page

--- src/startlist/startlist.tsx
+++ src/startlist/startlist.tsx
@@ -2,25 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Startlist = () => {
-  const [events, setEvents] = useState<any[]>([]);
+  const [entries, setEntries] = useState<any[]>([]);
 
-  const loadEvents = () => {
-    axios.get("/api/entries").then((res) => {
-      setEvents(res.data);
-    });
+  const loadEntries = () => {
+    axios
+      .get("/api/entries")
+      .then((res) => {
+        setEntries(res.data);
+      })
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    loadEvents();
+    loadEntries();
   }, []);
 
-  if (!events) return <h2>No events</h2>;
+  if (!entries) return <h2>No entries</h2>;
+  
   return (
     <div>
       <h2>Startlist</h2>
       <div>
-        {events.map((entry) => (
-          <p>{entry.firstName}</p>
+        {entries.map((entry) => (
+          <div>
+            <p>Status: {entry.status}</p>
+            <p>
+              {entry.firstName} {entry.firstName}
+            </p>
+            <p>{entry.eventTitle}</p>
+            <p>{entry.raceStartDate}</p>
+            <p>{entry.raceTitle}</p>
+            <p>Organised by {entry.organiserTitle}</p>
+            <p>Booking details:</p>
+            <p>Booked on {entry.bookingDate}</p>
+            <p>Ticket:</p>
+            <p>{entry.ticketTitle}</p>
+            <p>
+              {entry.ticketPrice.value} {entry.ticketPrice.currencyCode}
+            </p>
+            <p>
+              {entry.ticketPrice.fee} {entry.ticketPrice.currencyCode} fee
+            </p>
+          </div>
         ))}
       </div>
     </div>
