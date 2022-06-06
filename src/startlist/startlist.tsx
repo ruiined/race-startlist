d6a8955dc67baa379c862102077965ca7bd9aa6a
ruiined <ruiined@gmail.com>
2022-06-06 04:54:11 +0100

    Fixed the issue of ticket prices not being sorted

--- src/startlist/startlist.tsx
+++ src/startlist/startlist.tsx
@@ -22,6 +22,9 @@ const Startlist = () => {
   const handleOrgFilter = (event: React.ChangeEvent<HTMLSelectElement>) =>
     setFilters({ ...filters, organiserTitle: event.target.value });
 
+  const handleEvFilter = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    setFilters({ ...filters, eventTitle: event.target.value });
+
   const handleSort = (event: React.ChangeEvent<HTMLSelectElement>) =>
     setSort(event.target.value);
 
@@ -40,6 +43,16 @@ const Startlist = () => {
 
   const sortedEntries = !sort.length
     ? entries
+    : sort === "ticketPrice"
+    ? entries
+        .slice()
+        .sort((a, b) =>
+          a[sort]["value"] === b[sort]["value"]
+            ? 0
+            : a[sort]["value"] < b[sort]["value"]
+            ? position
+            : position * -1
+        )
     : entries
         .slice()
         .sort((a, b) =>
@@ -54,6 +67,17 @@ const Startlist = () => {
 
   const organisers = [...new Set(entries.map((entry) => entry.organiserTitle))];
 
+  const events = filters.organiserTitle
+    ? [
+        ...new Set(
+          entries.map((entry) => {
+            if (entry.organiserTitle === filters.organiserTitle)
+              return entry.eventTitle;
+          })
+        ),
+      ]
+    : [...new Set(entries.map((entry) => entry.eventTitle))];
+
   if (!entries) return <h2>No entries</h2>;
   if (error) return <h2>{error}</h2>;
 
@@ -67,15 +91,22 @@ const Startlist = () => {
         order={order}
         clearSort={clearSort}
       />
-      <select onChange={handleOrgFilter} name="filter" id="filter">
+      <select onChange={handleOrgFilter} name="orgFilter" id="orgFilter">
         {organisers.map((org, i) => (
           <option value={org} key={i}>
             {org}
           </option>
         ))}
       </select>
+      <select onChange={handleEvFilter} name="evFilter" id="evFilter">
+        {events.map((ev, i) => (
+          <option value={ev} key={i}>
+            {ev}
+          </option>
+        ))}
+      </select>
       <div>
-        {filteredEntries.map((entry, i) => (
+        {sortedEntries.map((entry, i) => (
           <div key={i}>
             <Entry entry={entry} />
           </div>
