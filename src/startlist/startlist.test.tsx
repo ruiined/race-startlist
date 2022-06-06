3131f0c921b1cd875d2ac0d2c98aa5db283c408e
ruiined <ruiined@gmail.com>
2022-06-06 07:17:30 +0100

    Component integration testing

--- src/startlist/startlist.test.tsx
+++ src/startlist/startlist.test.tsx
@@ -19,6 +19,58 @@ describe("Startlist", () => {
     mockedAxios.get.mockResolvedValueOnce(mockedResponse);
   });
 
+  it("displays options", async () => {
+    render(<Startlist />);
+    await waitFor(() => {
+      const print = screen.getByLabelText("print");
+      expect(print).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const print = screen.getByLabelText("Sort by");
+      expect(print).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const print = screen.getByLabelText("Reset sorting");
+      expect(print).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const print = screen.getByLabelText("filter events");
+      expect(print).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const print = screen.getByLabelText("filter organisations");
+      expect(print).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const print = screen.getByLabelText("reset filters");
+      expect(print).toBeInTheDocument();
+    });
+  });
+
+  it("displays the stats", async () => {
+    render(<Startlist />);
+    await waitFor(() => {
+      const totalRevenue = screen.getByText(/Total ticket revenue:/i);
+      expect(totalRevenue).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const totalRevenueCalculation = screen.getByText(/£6,500.00/i);
+      expect(totalRevenueCalculation).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const tickets = screen.getByText(/Tickets sold:/i);
+      expect(tickets).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const averagePrice = screen.getByText(/Average ticket price:/i);
+      expect(averagePrice).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const averagePriceCalculation = screen.getByText(/£3,250.00/i);
+      expect(averagePriceCalculation).toBeInTheDocument();
+    });
+  });
+
   it("displays event entries", async () => {
     render(<Startlist />);
     await waitFor(() => {
@@ -26,8 +78,28 @@ describe("Startlist", () => {
       expect(header).toBeInTheDocument();
     });
     await waitFor(() => {
-      const entry = screen.getByText(/asfdg/i);
-      expect(entry).toBeInTheDocument();
+      const firstName = screen.getByText(/asfdg/i);
+      expect(firstName).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const lastName = screen.getByText(/aasdf/i);
+      expect(lastName).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const status = screen.getByText(/asdf@asdf.asdf/i);
+      expect(status).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const raceStart = screen.getByText("12/09/2022 10:00");
+      expect(raceStart).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const ticketPrice = screen.getByText(/£4,300.00/i);
+      expect(ticketPrice).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      const ticketTitle = screen.getByText(/standard entry/i);
+      expect(ticketTitle).toBeInTheDocument();
     });
   });
 });
